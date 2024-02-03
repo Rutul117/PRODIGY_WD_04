1571be4932d1103fb87015d64049eb3362766555
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2024-02-03 17:39:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.multiple-text',{
+    strings: ["AI&DS Student", "Full-Stack Developer"],
+typeSpeed : 100,
+backSpeed : 40,
+bacDelay : 1000,
+loop:true,
+showCursor : false
+})
